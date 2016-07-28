0d46ac0220f17926314293403dc68f5fb1662fa5
ValentaTomas <valenta.and.thomas@gmail.com>
2016-07-28 18:13:02 +0200

    Release version 1

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-// =============================================================================
-// Algolia indexation node.js server
-// =============================================================================
-
-
-
 /* ----- Constants ----- */
-const ALGOLIA_API_KEY = process.env.ENV_ALGOLIA_API_KEY; // TODO heroku config
-const ALGOLIA_APP_ID = 'E6GMBD7AHH'; // TODO heroku config
-const ALGOLIA_INDEX = 'products'; // TODO heroku config
+const ALGOLIA_API_KEY = process.env.ENV_ALGOLIA_API_KEY;
+const ALGOLIA_APP_ID = 'E6GMBD7AHH';
+const ALGOLIA_INDEX = 'products';
 
 const FIREBASE_DATABASE_REF = '/products';
-var firebaseConfig = { // TODO heroku config
+var firebaseConfig = {
   apiKey: process.env.ENV_FIREBASE_API_KEY,
   authDomain: 'product-anatomy.firebaseapp.com',
   databaseURL: 'https://product-anatomy.firebaseio.com',
@@ -27,14 +21,11 @@ var client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
 var index = client.initIndex(ALGOLIA_INDEX);
 
 firebase.initializeApp(firebaseConfig);
-
 var fb = firebase.database().ref(FIREBASE_DATABASE_REF);
-var newItems = false;
 
-// Console logs
-console.log('\n=================================\n');
-console.log('Algolia indexation node.js server\n');
-console.log('=================================');
+var isNewItem = false;
+
+console.log('Initialization complete');
 /* ---------- */
 
 
@@ -45,29 +36,29 @@ fb.on('child_added', addOrUpdateObject);
 fb.on('child_changed', addOrUpdateObject);
 fb.on('child_removed', removeIndex);
 
-// Load the firebase database once on start, to prevent reindexation of existing data at algolia
-
+// Load the firebase database once on start to prevent reindex of existing data at algolia
 fb.once('value', function(dataSnapshot) {
   console.log('Initial data indexation skipped');
-  newItems = true;
+  isNewItem = true;
 });
-
 
 // Event functions
 function addOrUpdateObject(dataSnapshot) {
-  if(!newItems) return; // Initial re-indexation cancelation
+  // Initial re-index cancelation
+  if(isNewItem)
+  {
+    // Get Firebase object
+    var firebaseObject = dataSnapshot.val();
+    // Specify Algolia's objectID using the Firebase object key
+    firebaseObject.objectID = dataSnapshot.key;
 
-  // Get Firebase object
-  var firebaseObject = dataSnapshot.val();
-  // Specify Algolia's objectID using the Firebase object key
-  firebaseObject.objectID = dataSnapshot.key;
-
-  // Add or update object
-  index.saveObject(firebaseObject, function(err, content) {
-    if (err) {
-      console.log('Error: cannot save object in Algolia\n' + err);
-    }
-  });
+    // Add or update object
+    index.saveObject(firebaseObject, function(err, content) {
+      if (err) {
+        console.log('Error: cannot save/update object in Algolia\n' + err);
+      }
+    });
+  }
 }
 
 function removeIndex(dataSnapshot) {
@@ -77,7 +68,7 @@ function removeIndex(dataSnapshot) {
   // Remove the object from Algolia
   index.deleteObject(objectID, function(err, content) {
     if (err) {
-      console.log('Error: cannot delete object in Algolia\n' + err);
+      console.log('Error: cannot delete object from Algolia\n' + err);
     }
   });
 }
